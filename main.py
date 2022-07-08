8d64795fe24553532d75c4f7fff28a4c3ef68a69
Yousef <mr.ysaj@gmail.com>
2022-07-08 09:07:30 +0430

    all bug was fixed

---
+++ main.py
@@ -0,0 +1,29 @@
+import urllib.request
+import re
+import spacy
+
+target_url = 'https://www.gutenberg.org/files/95/95-0.txt'
+
+data = urllib.request.urlopen(target_url)
+raw_data = data.read().decode('utf8').strip()
+# Preprocess text into chapters
+chapters = re.sub('[^A-z0-9 -]', ' ', raw_data).split('CHAPTER')[1:]
+chapters[-1] = chapters[-1].split('End of the Project Gutenberg EBook')[0]
+
+
+# import spacy and load a NLP model
+
+nlp = spacy.load("en_core_web_lg", disable=["tagger", "parser"])
+# Analyze the first chapter
+c = chapters[0]
+# Get a list of persons
+doc = nlp(c)
+involved = list(set([ent.text for ent in doc.ents if ent.label_ == 'PERSON']))
+# replace names of involved in the text
+# with an id and save the mapping
+decode = dict()
+for i, x in enumerate(involved):
+    # Get mapping
+    decode['$${}$$'.format(i)] = x
+    # Preprocess text
+    c = c.replace(x, ' $${}$$ '.format(i))
